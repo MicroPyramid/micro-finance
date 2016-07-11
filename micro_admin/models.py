61179110df8a8d5ce32f2a855a090b6d1ff3c147
Shirisha Gaddi <sirisha.gaddi@micropyramid.com>
2016-07-11 09:43:53 +0530

    Fixed issues and updated day-book code
    Fixed issues and updated day-book code

--- micro_admin/models.py
+++ micro_admin/models.py
@@ -348,6 +348,9 @@ class Receipts(models.Model):
     def __unicode__(self):
         return self.receipt_number
 
+    def __str__(self):
+        return self.receipt_number
+
 
 class FixedDeposits(models.Model):
     client = models.ForeignKey(Client)
