7ba1ee0d1b45bf0942db9c1e947efa450e09ea16
Uday Teja <udayteja@micropyramid.com>
2017-10-12 18:57:17 +0530

    core app code updated with urls and views matching

--- core/urls.py
+++ core/urls.py
@@ -1,18 +1,18 @@
 from django.conf.urls import url
-from core.views import receipts_deposit, client_loan_accounts_view, get_loan_demands_view, PaySlipCreateView, get_group_loan_accounts, \
-    get_member_loan_accounts, get_fixed_deposit_accounts_view, get_recurring_deposit_accounts_view, ClientDepositAccountsView, \
-    GetFixedDepositPaidAccountsView, GetRecurringDepositPaidAccountsView
+from core.views import receipts_deposit, client_loan_accounts_view, get_loan_demands_view, payslip_create_view, get_group_loan_accounts, \
+    get_member_loan_accounts, get_fixed_deposit_accounts_view, get_recurring_deposit_accounts_view, client_deposit_accounts_view, \
+    get_fixed_deposit_paid_accounts_view, get_recurring_deposit_paid_accounts_view
 
 urlpatterns = [
     url(r'^receiptsdeposit/$', receipts_deposit, name="receiptsdeposit"),
     url(r'^getmemberloanaccounts/$', client_loan_accounts_view, name="getmemberloanaccounts"),
     url(r'^getloandemands/$', get_loan_demands_view, name="getloandemands"),
-    url(r'^payslip/$', PaySlipCreateView.as_view(), name="payslip"),
+    url(r'^payslip/$', payslip_create_view, name="payslip"),
     url(r'^loanaccounts/group$', get_group_loan_accounts, name="get_group_loan_accounts"),
     url(r'^loanaccounts/member$', get_member_loan_accounts, name="get_member_loan_accounts"),
     url(r'^getmemberfixeddepositaccounts/$', get_fixed_deposit_accounts_view, name="getmemberfixeddepositaccounts"),
     url(r'^getmemberrecurringdepositaccounts/$', get_recurring_deposit_accounts_view, name="getmemberrecurringdepositaccounts"),
-    url(r'^getmemberdepositaccounts/$', ClientDepositAccountsView.as_view(), name="getmemberdepositaccounts"),
-    url(r'^getmemberfixeddepositpaidaccounts/$', GetFixedDepositPaidAccountsView.as_view(), name="getmemberfixeddepositpaidaccounts"),
-    url(r'^getmemberrecurringdepositpaidaccounts/$', GetRecurringDepositPaidAccountsView.as_view(), name="getmemberrecurringdepositpaidaccounts"),
+    url(r'^getmemberdepositaccounts/$', client_deposit_accounts_view, name="getmemberdepositaccounts"),
+    url(r'^getmemberfixeddepositpaidaccounts/$', get_fixed_deposit_paid_accounts_view, name="getmemberfixeddepositpaidaccounts"),
+    url(r'^getmemberrecurringdepositpaidaccounts/$', get_recurring_deposit_paid_accounts_view, name="getmemberrecurringdepositpaidaccounts"),
 ]
